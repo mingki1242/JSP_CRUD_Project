2d563676be5455ec472d95ad66123ab44374c0d5
mingki1242 <qkralsrl214@naver.com>
2023-09-19 03:51:32 +0900

    first

---
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.io.*;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
+        out.println("<BODY><H2>Hello World: 나의 첫번째 Servlet 프로그램 </H2>");
+        out.println("오늘의 날짜와 시간: " + new Date());
+        out.println("</BODY></HTML>");
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException
+    {
+        doGet(request , response);
+    }
+    public void destroy() {
+    }
+}
